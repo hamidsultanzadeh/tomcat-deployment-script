01562278a4805ad7b6ff8f17c62259b3b352ad64
hamidsultanzadeh <sultanzade.06@mail.ru>
2022-09-06 05:58:20 +0400

    initial

---
+++ tomcat-deployment.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+ECHO Starting build project
+cd <PATH_TO_PROJECT>
+call <PATH_TO_MAVEN>\mvn -Dmaven.test.failure.ignore -Dmaven.test.skip=true clean package --U
+ECHO Build done
+ECHO Cleaning older deployments
+cd <PATH_TO_TOMCAT>\webapps
+del /f "PROJECT_NAME.(WAR OR JAR)"
+rmdir /s /Q RestVCC
+move <PATH_TO_PROJECT>\target\PROJECT_NAME.(WAR OR JAR) <PATH_TO_TOMCAT>\webapps\PROJECT_NAME.(WAR OR JAR)
+cd <PATH_TO_TOMCAT>\bin
+call catalina.bat run
+PAUSE
