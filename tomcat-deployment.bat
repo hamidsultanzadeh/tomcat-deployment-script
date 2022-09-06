1380403871746d9ad0c5af2d4f463470d9d062ac
hamidsultanzadeh <sultanzade.06@mail.ru>
2022-09-06 06:09:38 +0400

    change bat

--- tomcat-deployment.bat
+++ tomcat-deployment.bat
@@ -6,7 +6,7 @@ ECHO Build done
 ECHO Cleaning older deployments
 cd <PATH_TO_TOMCAT>\webapps
 del /f "PROJECT_NAME.(WAR OR JAR)"
-rmdir /s /Q RestVCC
+rmdir /s /Q PROJECT_NAME
 move <PATH_TO_PROJECT>\target\PROJECT_NAME.(WAR OR JAR) <PATH_TO_TOMCAT>\webapps\PROJECT_NAME.(WAR OR JAR)
 cd <PATH_TO_TOMCAT>\bin
 call catalina.bat run
